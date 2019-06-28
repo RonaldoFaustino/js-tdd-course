27b0ec5b449c8a45515f61a625a3cc946103babe
ronaldofaustino <ohnirucse@msn.com>
2019-06-27 22:51:44 -0300

    conf mocha

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,1 +1,33 @@
 
+describe('Main', function() {
+    describe('Method A', function() {
+        context('Case 1', function() {
+            it.skip('should happen', function() {
+                // espera que aconteça
+                // Entra de dados / metodo de soma(2,2)
+                // Espera que retorne (4)
+                throw new Error('just on error');
+            });
+        });
+
+        context.only('Case 2', function() {
+            it('should happen 2', function() {
+                // espera que aconteça
+                // Entra de dados / metodo de soma(2,2)
+                // Espera que retorne (4)
+                throw new Error('just on error 2');
+            });
+
+            it('should happen 3', function() {
+                // espera que aconteça
+                // Entra de dados / metodo de soma(2,2)
+                // Espera que retorne (4)
+
+            });
+        });
+    });
+
+    describe('Method B', function() {
+
+    });
+});
