a71ccffb64af4caf6e0f7c9e8c2789f4e14c07cd
Ronaldo Faustino Ferreira <ronaldo.FaustinoFerreira.terceiros@claro.com.b>
2019-09-08 20:47:24 -0300

    Trabalhando com hooks

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,1 +1,37 @@
+describe('Main', function() {
+    var arr;
+    //roda uma ve, antes do bloco
+    before(function() {
+      
+    });
 
+    // roda uma vez, depois do bloco
+    after(function() {
+        
+    });
+  
+    //roda todas as vezes, antes de cada bloco
+    beforeEach(function() {
+        arr = [1, 2 ,3]
+    });
+    
+    //roda todas as vezes, depois de cada bloco
+    afterEach(function() {
+        
+    });
+  
+    it('should have of 4 when push another value to the array', function(){
+        arr.push(4);
+        console.log(arr.length);
+
+    })
+
+    it('should have  a size  of 2 when pop  a value from the array', function(){
+        arr.pop();
+        console.log(arr.length);
+    })
+
+    it('should remove the value 3 when use pop in the array', function(){
+        console.log(arr.pop() === 3);
+    })
+  });
